35b9bb3926a0154cc1f5a7be008413a54bab5d87
zeoxm <zzeoxm@gmail.com>
2025-07-04 11:58:07 +0200

    Update generate_xlsx.py

--- generate_xlsx.py
+++ generate_xlsx.py
@@ -7,16 +7,16 @@ def generate_synthese_manager(json_data_list, output_path):
             "Chatteur": data.get("chatteur"),
             "Modèle": data.get("modele"),
             "SPC": data.get("SPC"),
-            "Sales NET": round(data.get("Sales", 0), 2),
+            "Sales NET": round(data.get("Sales") or 0, 2),
             "$/h": data.get("$/h"),
             "Typo 1": data.get("typologies")[0] if data.get("typologies") else "",
             "Typo 2": data.get("typologies")[1] if len(data.get("typologies")) > 1 else "",
             "Flags": ", ".join(data.get("flags")) if data.get("flags") else "",
             "Axe": data.get("axe"),
             "Modules": ", ".join([str(m) for m in data.get("modules")]) if data.get("modules") else "",
-            "Salaire brut": round(data.get("salaire_brut"), 2),
+            "Salaire brut": round(data.get("salaire_brut") or data.get ("salaire") or 0, 2),
             "Ajustement": data.get("ajustement", 0),
-            "Salaire net": round(data.get("salaire_net"), 2),
+            "Salaire net": round(data.get("salaire_net") or data.get ("salaire") or 0, 2),
             "Appel ?": data.get("appel_managérial")
         }
         rows.append(row)
